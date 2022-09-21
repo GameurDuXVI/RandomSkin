8f8714f023eea266ce6611e6704f2dac361f404b
GameurDuXVI <gameur.codeur@gmail.com>
2022-09-21 14:37:10 +0200

    Commit final result

--- launch.bat
+++
@@ -1,3 +0,0 @@
-::echo "%JAVA_HOME%\bin\java.exe" -jar ./target/RandomSkin-jar-with-dependencies.jar
-"C:\Program Files\Java\jdk-17.0.1\bin\java.exe" -jar ./target/RandomSkin-jar-with-dependencies.jar
-pause
